8cf8dc3027276c836e429fcf81cba337e111e882
heyethereum <heyethereum@gmail.com>
2022-09-24 23:53:53 +0800

    useNavigate hook

--- react-js/my-app/src/pages/SingleProduct.js
+++ react-js/my-app/src/pages/SingleProduct.js
@@ -1,14 +1,22 @@
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import products from "../data";
 const SingleProduct = () => {
   const { productId } = useParams();
   const product = products.find((product) => product.id === productId);
   const { image, name } = product;
+  const navigate = useNavigate();
   return (
     <section className="section product">
       <img src={image} alt={name} />
       <h5>{name}</h5>
-      <Link to="/products">back to products</Link>
+      <div
+        className="btn"
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        back to products
+      </div>
     </section>
   );
 };
