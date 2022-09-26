e3db3bf524e3af8157d36511a2ecfb336155164a
heyethereum <heyethereum@gmail.com>
2022-09-26 22:20:20 +0800

    change css to flex wrap

--- react-js/my-app/src/pages/AllUsers.js
+++ react-js/my-app/src/pages/AllUsers.js
@@ -5,22 +5,25 @@ import User from "./User";
 const AllUsers = () => {
   const baseURL = `https://reqres.in/api/users`;
   const [url, setUrl] = useState(baseURL);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
   const { data } = useFetchData(url);
   const users = data.data;
 
-  console.log(users);
+  const handleChange = (e) => {
+    setUrl(`${baseURL}?page=${e.target.value}`);
+    setUser(null);
+  };
 
   return (
     <section className="section">
       <div>
-        <select onChange={(e) => setUrl(`${baseURL}?page=${e.target.value}`)}>
+        <select onChange={handleChange}>
           <option value="1">Page 1</option>
           <option value="2">Page 2</option>
           <option value="3">Page 3</option>
         </select>
       </div>
-      <div className="container">
+      <div className="container-flex">
         {users?.length === 0 ? <h5>No Users Found!</h5> : ""}
         {user && <User user={user} />}
         {users?.map((user) => {
