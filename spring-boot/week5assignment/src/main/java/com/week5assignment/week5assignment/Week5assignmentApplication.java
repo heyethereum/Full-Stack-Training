fccf3d05aa46efd76388a8ee32b44b80aa1629d2
heyethereum <heyethereum@gmail.com>
2022-10-11 11:29:40 +0800

    added .env configurations

--- spring-boot/week5assignment/src/main/java/com/week5assignment/week5assignment/Week5assignmentApplication.java
+++ spring-boot/week5assignment/src/main/java/com/week5assignment/week5assignment/Week5assignmentApplication.java
@@ -1,12 +1,37 @@
 package com.week5assignment.week5assignment;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.StandardEnvironment;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication
 public class Week5assignmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Week5assignmentApplication.class, args);
+		// SpringApplication.run(Week5assignmentApplication.class, args);
+
+		Map<String, Object> env = Dotenv.load()
+				.entries()
+				.stream()
+				.collect(
+						Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
+		new SpringApplicationBuilder(
+				Week5assignmentApplication.class)
+				.environment(new StandardEnvironment() {
+					@Override
+					protected void customizePropertySources(MutablePropertySources propertySources) {
+						super.customizePropertySources(propertySources);
+						propertySources.addLast(new MapPropertySource("dotenvProperties", env));
+					}
+				}).run(args);
 	}
 }
