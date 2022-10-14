3a2411ac826d05b512468dfecce4075e9085b557
heyethereum <heyethereum@gmail.com>
2022-10-14 22:58:54 +0800

    Solved React CORS Error with CORS Interceptor

--- spring-boot/week5assignment/src/main/java/com/week5assignment/week5assignment/config/SpringConfig.java
+++ spring-boot/week5assignment/src/main/java/com/week5assignment/week5assignment/config/SpringConfig.java
@@ -17,8 +17,7 @@ public class SpringConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedHeaders("*").allowedMethods("*");
-
+    registry.addMapping("/**").allowedOrigins("http://localhost:3000");
   }
 
   @Override
